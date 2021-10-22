39d22a118669502b38d31517684f525ec6921797
Rudz1 <nobloko80@gmail.com>
2021-10-22 01:42:04 -0300

    add a middleware auth

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts', [PostController::class, 'list'])->name('posts.list');
-Route::get('/posts/create-form', [PostController::class, 'createForm'])->name('posts.create-form');
-Route::post('/posts/create-save', [PostController::class, 'create'])->name('posts.create-save');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts', [PostController::class, 'list'])->name('posts.list');
+    Route::get('/posts/create-form', [PostController::class, 'createForm'])->name('posts.create-form');
+    Route::post('/posts/create-save', [PostController::class, 'create'])->name('posts.create-save');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
